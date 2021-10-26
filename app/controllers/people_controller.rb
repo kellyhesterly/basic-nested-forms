02c32bbe9e2995b1bd95cdd2a9a9cc3cb4ad8260
github username <kelly.hesterly@gmail.com>
2021-10-25 22:11:13 -0600

    commit tree 578cb8beddd1e539d66b78daa547214cbe0dbf07

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,6 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
-    @person.addresses.build(address_type: 'work')
-    @person.addresses.build(address_type: 'home')
   end
 
   def create    
@@ -17,16 +15,6 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(
-      :name,
-      addresses_attributes: [
-        :street_address_1,
-        :street_address_2,
-        :city,
-        :state,
-        :zipcode,
-        :address_type
-      ]
-    )
+    params.require(:person).permit(:name)
   end
 end
